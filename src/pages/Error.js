7e5c2e4d88fef5d0633b2cca440c2afd996509c0
neelkanth <nl543396@dal.ca>
2020-08-14 23:20:12 -0300

    Added error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return <Wrapper>
+    <div>
+      <h1>404</h1>
+      <h3>Page Not Found :(</h3>
+      <Link to="/" className="btn">
+        Redirect to Home
+      </Link>
+    </div>
+  </Wrapper>
 };
 const Wrapper = styled.section`
   min-height: 100vh;
