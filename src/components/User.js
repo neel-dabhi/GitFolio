aa7693a334b92a63a1e71ef03547ff6f1064cbf4
neelkanth <nl543396@dal.ca>
2020-08-19 15:24:24 -0300

    removed Followers.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Card from './Card';
-import Followers from './Followers';
+
 const User = () => {
   return <section className="section">
     <Wrapper className="section-center">
