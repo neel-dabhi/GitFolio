49ec3a5d68c96f807624c65d0967457426980719
neel-dabhi <yoursdabhineelkanth@gmail.com>
2025-01-17 11:30:00 -0400

    fix: remove unused axios import and add homepage field

--- src/components/GitHubCalendar.js
+++ src/components/GitHubCalendar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {GithubContext} from "../context/context";
 import styled from "styled-components";
-import axios from 'axios';
 
 const GitHubCalendarGrid = () => {
     const {githubUser} = React.useContext(GithubContext); // Moved to top level
